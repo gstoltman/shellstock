cc866f424822e9b8bf7c53318f90e1919e4f87d3
gstoltman <gstoltman@gmail.com>
2024-04-06 14:29:15 -0700

    adding shell.nix

---
+++ shell.nix
@@ -0,0 +1,14 @@
+# shell.nix
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: [
+      # select Python packages here
+      python-pkgs.pandas
+      python-pkgs.matplotlib
+      python-pkgs.requests
+      python-pkgs.pip
+    ]))
+  ];
+}
